ef78f9587e5473fc237bc93da69169e543bf0730
Florian Marending <florian@marending.dev>
2026-04-03 14:29:25 +0200

    Merge fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias node="/opt/javascript/ncanode/22.18.0/node/bin/node"
-    set NVM_DIR "$HOME/.nvm"
-    /opt/homebrew/opt/nvm/nvm.sh
 
-    zoxide init fish | source
 end
+
+set fish_greeting
+
+zoxide init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
